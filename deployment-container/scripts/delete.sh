86e29138b1f51f8ee65cd914cc22de92c6f5798d
Louis-Guillaume MORAND <lgm@lgmorand.com>
2024-05-15 18:31:21 +0000

    wip

--- deployment-container/scripts/delete.sh
+++ deployment-container/scripts/delete.sh
@@ -1,36 +1,42 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+set -e # exit on error
+
+EnvironmentState="$ADE_STORAGE/environment.tfstate"
+EnvironmentPlan="/environment.tfplan"
+EnvironmentVars="/environment.tfvars.json"
+
+echo "$ADE_OPERATION_PARAMETERS" > $EnvironmentVars
+
+# Set up Terraform AzureRM managed identity.
 export ARM_USE_MSI=true
 export ARM_CLIENT_ID=$ADE_CLIENT_ID
 export ARM_TENANT_ID=$ADE_TENANT_ID
 export ARM_SUBSCRIPTION_ID=$ADE_SUBSCRIPTION_ID
 
+if ! test -f $EnvironmentState; then
+    echo "No state file present. Delete succeeded."
+    exit 0
+fi
+
+echo -e "\n>>> Terraform...\n"
+echo -e "\n>>> Terraform Info...\n"
+terraform -version
+
+echo -e "\n>>> Initializing Terraform...\n"
+terraform init -no-color
+
+echo -e "\n>>> Creating Terraform Plan...\n"
 export TF_VAR_resource_group_name=$ADE_RESOURCE_GROUP_NAME
 export TF_VAR_ade_env_name=$ADE_ENVIRONMENT_NAME
 export TF_VAR_env_name=$ADE_ENVIRONMENT_NAME
 export TF_VAR_ade_subscription=$ADE_SUBSCRIPTION_ID
 export TF_VAR_ade_location=$ADE_ENVIRONMENT_LOCATION
 export TF_VAR_ade_environment_type=$ADE_ENVIRONMENT_TYPE
-# we can add other ENV from https://learn.microsoft.com/en-us/azure/deployment-environments/reference-deployment-environment-variables
-
-terraform init
 terraform plan -no-color -compact-warnings -destroy -refresh=true -lock=true -state=$EnvironmentState -out=$EnvironmentPlan -var-file="$EnvironmentVars"
+
+echo -e "\n>>> Applying Terraform Plan...\n"
 terraform apply -no-color -compact-warnings -auto-approve -lock=true -state=$EnvironmentState $EnvironmentPlan
-
-tfOutputs=$(terraform output -state=$EnvironmentState -json)
-# Convert Terraform output format to ADE format.
-tfOutputs=$(jq 'walk(if type == "object" then 
-            if .type == "bool" then .type = "boolean" 
-            elif .type == "list" then .type = "array" 
-            elif .type == "map" then .type = "object" 
-            elif .type == "set" then .type = "array" 
-            elif (.type | type) == "array" then 
-                if .type[0] == "tuple" then .type = "array" 
-                elif .type[0] == "object" then .type = "object" 
-                elif .type[0] == "set" then .type = "array" 
-                else . 
-                end 
-            else . 
-            end 
-        else . 
-        end)' <<< "$tfOutputs")
-
-echo "{\"outputs\": $tfOutputs}" > $ADE_OUTPUTS
